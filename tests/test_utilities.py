2e59b986f771b414774529ab3fe8037c334f7854
Derek Wan <d.wan@icloud.com>
2020-04-30 10:29:45 +0800

    Introduce "map_dict", generalize "map" (#23)
    * Introduce "dict" argument to map
    
    * WIP, starmap built into map
    
    * Use lists in strategies, part 1
    
    * Lists part 2
    
    * WIP more tests using lists
    
    * Finish cleaning tests
    
    * Remove .ordered
    
    * Remove crap in cattrs
    
    * Fix annotation in map
    
    * Simplify helper_map_dict
    
    * Reorder
    
    * Bump version: 0.9.19 → 0.9.20

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from itertools import chain
 from operator import mod
+from operator import neg
+from typing import Tuple
 
 from functional_itertools.utilities import sentinel
 
@@ -11,3 +14,11 @@ def test_sentinel() -> None:
 
 def is_even(x: int) -> bool:
     return mod(x, 2) == 0
+
+
+def neg_key_and_value(key: int, value: int) -> Tuple[int, int]:
+    return neg(key), neg(value)
+
+
+def sum_varargs(x: int, *xs: int) -> int:
+    return sum(chain([x], xs))
