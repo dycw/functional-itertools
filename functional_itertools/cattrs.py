d8b9bb8cc0c60af633fc2e7a9ea6bc231ac32bf3
Derek Wan <d.wan@icloud.com>
2020-04-30 20:03:52 +0800

    Finish tests

--- functional_itertools/cattrs.py
+++ functional_itertools/cattrs.py
@@ -30,7 +30,7 @@ class CAttrs(Generic[T]):
         recurse: bool = True,
         filter: Optional[Callable[[Attribute, Any], bool]] = None,  # noqa: A002
     ) -> CDict[str, T]:
-        return asdict(self, recurse=recurse, filter=filter, dict_factory=CDict)
+        return helper_cattrs_dict(self, recurse=recurse, filter=filter)
 
     def map(  # noqa: A003
         self: CAttrs[T],
@@ -38,23 +38,44 @@ class CAttrs(Generic[T]):
         parallel: bool = False,
         processes: Optional[int] = None,
         recurse: bool = True,
+        filter: Optional[Callable[[Attribute, Any], bool]] = None,  # noqa: A002
     ) -> CAttrs[U]:
         return helper_cattrs_map(
-            self, func, parallel=parallel, processes=processes, recurse=recurse,
+            self, func=func, parallel=parallel, processes=processes, recurse=recurse, filter=filter,
         )
 
 
-def helper_cattrs_map(
-    value: Any,
-    func: Callable[..., U],
+def helper_cattrs_dict(
+    x: Any,
     *,
+    recurse: bool = True,
+    filter: Optional[Callable[[Attribute, Any], bool]] = None,  # noqa: A002
+) -> Any:
+    if isinstance(x, CAttrs):
+        res: CDict[T] = asdict(x, recurse=False, filter=filter, dict_factory=CDict)
+        if recurse:
+            return res.map_values(partial(helper_cattrs_dict, recurse=recurse, filter=filter))
+        else:
+            return res
+    else:
+        return x
+
+
+def helper_cattrs_map(
+    x: Any,
+    *,
+    func: Callable[..., U],
     parallel: bool = False,
     processes: Optional[int] = None,
     recurse: bool = True,
+    filter: Optional[Callable[[Attribute, Any], bool]] = None,  # noqa: A002
 ) -> Any:
-    if isinstance(value, CAttrs):
+    if isinstance(x, CAttrs):
         not_attr_items, is_attr_items = (
-            value.dict(recurse=False).items().partition(lambda x: has(x[1])).map(lambda x: x.dict())
+            x.dict(recurse=False, filter=filter)
+            .items()
+            .partition(lambda x: has(x[1]))
+            .map(lambda x: x.dict())
         )
         if recurse:
             extra = is_attr_items.map_values(
@@ -64,10 +85,11 @@ def helper_cattrs_map(
                     parallel=parallel,
                     processes=processes,
                     recurse=recurse,
+                    filter=filter,
                 ),
             )
         else:
             extra = is_attr_items
-        return evolve(value, **not_attr_items.map_values(func), **extra)
+        return evolve(x, **not_attr_items.map_values(func), **extra)
     else:
-        return func(value)
+        return func(x)
--- tests/classes/test_cattrs.py
+++ tests/classes/test_cattrs.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
 from operator import neg
+from typing import Any
+from typing import Callable
+from typing import Dict
 from typing import TypeVar
 
+from attr import Attribute
 from attr import attrs
+from pytest import mark
 
 from functional_itertools import CAttrs
 from functional_itertools import CDict
@@ -32,23 +37,53 @@ def test_dict_simple() -> None:
     assert x == {"a": 1, "b": 2, "c": 3}
 
 
-def test_dict_with_recurse() -> None:
-    x = Foo(a=1, b=2, c=Bar(d=3, e=4, f=5)).dict(recurse=True)
+@mark.parametrize(
+    "recurse, expected",
+    [
+        (True, {"a": 1, "b": 2, "c": {"d": 3, "e": 4, "f": 5}}),
+        (False, {"a": 1, "b": 2, "c": Bar(d=3, e=4, f=5)}),
+    ],
+)
+def test_dict_nested_recurse(recurse: bool, expected: Dict[str, Any]) -> None:
+    x = Foo(a=1, b=2, c=Bar(d=3, e=4, f=5)).dict(recurse=recurse)
     assert isinstance(x, CDict)
-    assert x == {"a": 1, "b": 2, "c": {"d": 3, "e": 4, "f": 5}}
-    assert isinstance(x["c"], CDict)
+    assert x == expected
 
 
-def test_dict_without_recurse() -> None:
-    x = Foo(a=1, b=2, c=Bar(d=3, e=4, f=5)).dict(recurse=False)
+@mark.parametrize(
+    "filter, expected",
+    [
+        (lambda k, v: k.name == "a", {"a": 1}),
+        (lambda k, v: v >= 2, {"b": 2, "c": 3}),
+        (lambda k, v: k.name == "a" or v == 3, {"a": 1, "c": 3}),
+    ],
+)
+def test_dict_simple_filter(
+    filter: Callable[[Attribute, Any], bool], expected: Dict[str, Any],  # noqa: A002
+) -> None:
+    x = Foo(a=1, b=2, c=3).dict(filter=filter)
     assert isinstance(x, CDict)
-    assert x == {"a": 1, "b": 2, "c": Bar(d=3, e=4, f=5)}
+    assert x == expected
 
 
-def test_dict_with_filter() -> None:
-    x = Foo(a=1, b=2, c=3).dict(filter=lambda k, v: k.name == "a" or v == 2)
+@mark.parametrize(
+    "filter, expected",
+    [
+        (lambda k, v: k.name == "a", {"a": 1}),
+        (lambda k, v: k.name == "c", {"c": {}}),
+        (lambda k, v: isinstance(v, int) and (v >= 2), {"b": 2}),
+        (
+            lambda k, v: (isinstance(v, int) and ((v <= 1) or (v >= 5))) or isinstance(v, Bar),
+            {"a": 1, "c": {"f": 5}},
+        ),
+    ],
+)
+def test_dict_nested_filter(
+    filter: Callable[[Attribute, Any], bool], expected: Dict[str, Any],  # noqa: A002
+) -> None:
+    x = Foo(a=1, b=2, c=Bar(d=3, e=4, f=5)).dict(filter=filter)
     assert isinstance(x, CDict)
-    assert x == {"a": 1, "b": 2}
+    assert x == expected
 
 
 def test_map_simple() -> None:
@@ -57,13 +92,14 @@ def test_map_simple() -> None:
     assert x == Foo(a=-1, b=-2, c=-3)
 
 
-def test_map_with_recurse() -> None:
-    x = Foo(a=1, b=2, c=Bar(d=3, e=4, f=5)).map(neg)
+@mark.parametrize(
+    "recurse, expected",
+    [
+        (True, Foo(a=-1, b=-2, c=Bar(d=-3, e=-4, f=-5))),
+        (False, Foo(a=-1, b=-2, c=Bar(d=3, e=4, f=5))),
+    ],
+)
+def test_map_nested(recurse: bool, expected: Any) -> None:
+    x = Foo(a=1, b=2, c=Bar(d=3, e=4, f=5)).map(neg, recurse=recurse)
     assert isinstance(x, Foo)
-    assert x == Foo(a=-1, b=-2, c=Bar(d=-3, e=-4, f=-5))
-
-
-def test_map_without_recurse() -> None:
-    x = Foo(a=1, b=2, c=Bar(d=3, e=4, f=5)).map(neg, recurse=False)
-    assert isinstance(x, Foo)
-    assert x == Foo(a=-1, b=-2, c=Bar(d=3, e=4, f=5))
+    assert x == expected
